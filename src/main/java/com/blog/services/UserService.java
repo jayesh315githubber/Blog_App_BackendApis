9b1a2453d5a92d792c207d8069d49cdb90cb3fd4
jayesh315githubber <jayeshgangurde315@gmail.com>
2023-06-05 19:18:52 +0530

    My Initial commit

---
+++ src/main/java/com/blog/services/UserService.java
@@ -0,0 +1,23 @@
+package com.blog.services;
+
+import java.util.List;
+
+import com.blog.payloads.UserDto;
+
+public interface UserService {
+	
+	
+	UserDto createUser(UserDto user);
+	
+	UserDto updateUser(UserDto user, Integer userId);
+	
+	UserDto getUserById(Integer userId);
+	
+	List<UserDto> getAllUsers();
+	
+	void deteleUser(Integer userId);
+	
+	
+	
+
+}
---
+++ src/test/java/com/jayeshgangurde/blog/BlogingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jayeshgangurde.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
