861dbf4d59f00d009b52f0a28ca589586fd1c424
jayesh315githubber <jayeshgangurde315@gmail.com>
2023-06-06 19:28:06 +0530

    feat - spring boot validator is added

--- src/main/java/com/blog/controllers/UserController.java
+++ src/main/java/com/blog/controllers/UserController.java
@@ -1,11 +1,11 @@
 package com.blog.controllers;
 
 import java.util.List;
-import java.util.Map;
-
+import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,27 +14,29 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.blog.payloads.ApiResponse;
 import com.blog.payloads.UserDto;
 import com.blog.services.UserService;
 
 @RestController
+@Validated
 @RequestMapping("/api/users")
 public class UserController {
 
 	@Autowired
 	private UserService userService;
 	
+//	 @Valid annotation to enable the validation on UserDto
 	@PostMapping("/")
-	public ResponseEntity<UserDto> createUser(@RequestBody UserDto userDto){
+	public ResponseEntity<UserDto> createUser(@Valid @RequestBody UserDto userDto){
 		
 		UserDto createUserDto = this.userService.createUser(userDto);
 		return new ResponseEntity<>(createUserDto,HttpStatus.CREATED);
 	}
 	
+//		 @Valid annotation to enable the validation on UserDto
 	@PutMapping("/{userId}")
-	public ResponseEntity<UserDto> updateUser(@RequestBody UserDto userDto, @PathVariable("userId") Integer uid){
+	public ResponseEntity<UserDto> updateUser( @Valid @RequestBody  UserDto userDto, @PathVariable("userId") Integer uid){
 		
 		UserDto updatedUser = userService.updateUser(userDto, uid);
 		return ResponseEntity.ok(updatedUser);
